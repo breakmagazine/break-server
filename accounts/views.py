ab20641405c1037811b45c47e9180d772101a3e0
yjoonjang <yjoonjang34@gmail.com>
2024-01-24 03:30:40 +0900

    refactor: 공통 컴포넌트 함수화 및 리팩토링

--- accounts/views.py
+++ accounts/views.py
@@ -3,76 +3,75 @@ from django.db import transaction
 import requests
 from django.shortcuts import redirect
 from django.conf import settings
-from django.utils.translation import gettext_lazy as _
-from django.http import JsonResponse
 from rest_framework import status
 from json.decoder import JSONDecodeError
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from dj_rest_auth.registration.views import SocialLoginView
-from allauth.socialaccount.providers.kakao import views as kakao_view
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-from allauth.socialaccount.models import SocialAccount
 
 from breakserver.settings import secrets
 from .adapter import CustomKakaoOAuth2Adapter
 from .models import CustomUser
-from drf_yasg.utils import swagger_auto_schema
-from drf_yasg import openapi
-
 
 BASE_URL = 'http://127.0.0.1:8000/'
 KAKAO_CALLBACK_URI = BASE_URL + 'accounts/kakao/callback/'
+REST_API_KEY = getattr(settings, 'KAKAO_REST_API_KEY')
+CLIENT_SECRET = secrets['SECRET_KEY']
+
+# Kakao API 요청 함수
+def get_kakao_token(code):
+    try:
+        response = requests.get(
+            f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={REST_API_KEY}&client_secret={CLIENT_SECRET}&redirect_uri={KAKAO_CALLBACK_URI}&code={code}"
+        )
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        raise e
+
+# Kakao 프로필 정보 요청 함수
+def get_kakao_profile(access_token):
+    try:
+        response = requests.get(
+            "https://kapi.kakao.com/v2/user/me",
+            headers={"Authorization": f"Bearer {access_token}"}
+        )
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        raise e
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def kakao_login(request):
-    rest_api_key = getattr(settings, 'KAKAO_REST_API_KEY')
     return redirect(
-        f"https://kauth.kakao.com/oauth/authorize?client_id={rest_api_key}&redirect_uri={KAKAO_CALLBACK_URI}&response_type=code"
+        f"https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri={KAKAO_CALLBACK_URI}&response_type=code"
     )
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def kakao_callback(request):
-    rest_api_key = getattr(settings, 'KAKAO_REST_API_KEY')
-    code = request.GET.get("code")
-    redirect_uri = KAKAO_CALLBACK_URI
-    client_secret = secrets['SECRET_KEY']
-
     # Access Token Request
     try:
-        token_req = requests.get(
-            f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={rest_api_key}&client_secret={client_secret}&redirect_uri={redirect_uri}&code={code}"
-        )
-        token_req_json = token_req.json()
-        error = token_req_json.get("error")
-
-        if error is not None:
-            return Response({'error': error}, status=status.HTTP_400_BAD_REQUEST)
-
-        access_token = token_req_json.get("access_token")
-        refresh_token = token_req_json.get("refresh_token")
+        code = request.GET.get("code")
+        token_data = get_kakao_token(code)
+        access_token = token_data.get("access_token")
+        refresh_token = token_data.get("refresh_token")
 
         # Profile Request
-        profile_request = requests.get(
-            "https://kapi.kakao.com/v2/user/me", headers={"Authorization": f"Bearer {access_token}"}
-        )
-        profile_json = profile_request.json()
-
-        user_oid = profile_json.get('id')
-        kakao_account = profile_json.get('kakao_account')
+        profile_data = get_kakao_profile(access_token)
+        user_oid = profile_data.get('id')
+        kakao_account = profile_data.get('kakao_account')
         nickname = kakao_account['profile']['nickname']
         profile_image_url = kakao_account['profile']['profile_image_url']
-
 
         # Signup or Signin Request
         try:
             user = CustomUser.objects.get(oid=user_oid)
 
             #user가 존재할 때 아래 데이터 반환
-
             response_data = {
                 'user_id': user.oid,
                 'username': user.username,
