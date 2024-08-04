96e164ade766a5e547e9846566617b35109900cf
yjoonjang <yjoonjang34@gmail.com>
2024-08-05 00:35:01 +0900

    feat: article update, delete api 추가

--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ArticleCreateView, ArticleDetailView, ArticleListByCategoryView
+from .views import ArticleCreateView, ArticleDetailView, ArticleListByCategoryView, ArticleUpdateView, ArticleDeleteView
 
 urlpatterns = [
     path("", ArticleCreateView.as_view(), name="article-create"),
     path("<int:pk>/", ArticleDetailView.as_view(), name="article-detail"),
     path("all/", ArticleListByCategoryView.as_view(), name="article-list-by-category"),
+    path('<int:pk>/update/', ArticleUpdateView.as_view(), name='article-update'),
+    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article-delete')
 ]
